5864aedee4fc394c20af4d3386e8b06575e11b53
WithLithum <WithLithum@outlook.com>
2026-02-12 20:33:06 +0800

    ♻️ Remove player saving logic from PlayerManager

--- src/main/java/x/withlithum/neoware/server/player/PlayerManagerImpl.java
+++ src/main/java/x/withlithum/neoware/server/player/PlayerManagerImpl.java
@@ -6,12 +6,9 @@
 package x.withlithum.neoware.server.player;
 
 import lombok.extern.slf4j.Slf4j;
-import net.kyori.adventure.nbt.BinaryTagIO;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.adventure.audience.Audiences;
-import net.minestom.server.codec.Result;
-import net.minestom.server.codec.Transcoder;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
@@ -22,8 +19,6 @@ import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.network.player.GameProfile;
 import net.minestom.server.network.player.PlayerConnection;
 import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
-import x.withlithum.neoware.data.player.PlayerInfo;
 import x.withlithum.neoware.data.storage.PlayerRecorder;
 import x.withlithum.neoware.instance.ManagedInstance;
 import x.withlithum.neoware.server.security.BanManager;
@@ -31,9 +26,6 @@ import x.withlithum.neoware.util.messages.BanMessage;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 @Slf4j
 @NullMarked
@@ -42,16 +34,10 @@ public final class PlayerManagerImpl implements PlayerManager {
     private final ManagedInstance instance;
     private final PlayerRecorder recorder;
 
-    private final Path playersDir;
-    private final ConcurrentMap<UUID, PlayerInfo> infoCache = new ConcurrentHashMap<>();
-
     public PlayerManagerImpl(BanManager banManager, ManagedInstance instance, PlayerRecorder recorder, Path playersDir) {
         this.banManager = banManager;
         this.instance = instance;
         this.recorder = recorder;
-
-        log.info("Store player data into: {}", playersDir);
-        this.playersDir = playersDir;
 
         try {
             if (!Files.isDirectory(playersDir)) {
@@ -60,38 +46,6 @@ public final class PlayerManagerImpl implements PlayerManager {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private Path getPlayerFile(UUID uuid) {
-        return playersDir.resolve(String.format("%s.dat", uuid));
-    }
-
-    private @Nullable PlayerInfo getPlayerInfo(UUID uuid) {
-        return infoCache.computeIfAbsent(uuid, _ -> {
-            var path = getPlayerFile(uuid);
-            if (!Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS)) {
-                return null;
-            }
-
-            Result<PlayerInfo> decodeResult;
-            try {
-                decodeResult = PlayerInfo.CODEC.decode(Transcoder.NBT, BinaryTagIO.reader().read(path));
-            } catch (IOException e) {
-                log.warn("Failed to load player info for player with UUID {}", path);
-                log.warn("details: ", e);
-                return null;
-            }
-
-            switch (decodeResult) {
-                case Result.Ok<PlayerInfo>(PlayerInfo v) -> {
-                    return v;
-                }
-                case Result.Error<PlayerInfo>(String message) -> {
-                    log.warn("Failed to decode player info for player with UUID {}: {}", uuid, message);
-                    return null;
-                }
-            }
-        });
     }
 
     @Override
@@ -113,16 +67,11 @@ public final class PlayerManagerImpl implements PlayerManager {
     @Override
     public void configure(AsyncPlayerConfigurationEvent config) {
         var player = config.getPlayer();
-        var data = getPlayerInfo(player.getUuid());
-        config.setSpawningInstance(instance.getInstance());
-
-        if (data == null) {
-            player.setRespawnPoint(new Pos(-251, -17, 142));
-            return;
-        }
-
-        // Preload player data from recorder
+        // Preload player data from recorder.
         recorder.preRewindPlayer(player);
+
+        config.setSpawningInstance(instance.getInstance());
+        player.setRespawnPoint(new Pos(-251, -17, 142));
 
         log.info("Player {} joined with UUID {}", player.getUsername(), player.getUuid());
 
