587365a225425fc2dd1fff36e7c4d0c0e54b951e
WithLithum <WithLithum@outlook.com>
2026-02-12 11:29:48 +0800

    🐛 Fix equipment slot groups not serialized properly

--- src/main/kotlin/x/withlithum/neoware/data/serial/EquipmentSlotGroupSerializer.kt
+++ src/main/kotlin/x/withlithum/neoware/data/serial/EquipmentSlotGroupSerializer.kt
@@ -13,6 +13,7 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import net.minestom.server.entity.EquipmentSlotGroup
+import java.util.Locale
 
 object EquipmentSlotGroupSerializer : KSerializer<EquipmentSlotGroup> {
     override val descriptor: SerialDescriptor
@@ -28,8 +29,14 @@ object EquipmentSlotGroupSerializer : KSerializer<EquipmentSlotGroup> {
     override fun deserialize(decoder: Decoder): EquipmentSlotGroup {
         val string = decoder.decodeString()
 
+        when (string) {
+            "mainhand" -> return EquipmentSlotGroup.MAIN_HAND
+            "offhand" -> return EquipmentSlotGroup.OFF_HAND
+            "main_hand", "off_hand" -> throw SerializationException("Unacceptable equipment slot group '$string'")
+        }
+
         try {
-            return EquipmentSlotGroup.valueOf(string)
+            return EquipmentSlotGroup.valueOf(string.uppercase(Locale.ROOT))
         } catch (e: IllegalArgumentException) {
             throw SerializationException("Invalid equipment slots group string: $string", e)
         }
